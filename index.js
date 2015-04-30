3f471b102fc33ac6a053e35554ac8e0627828237
Talal Balouch <talal.balouch.ctr@darpa.mil>
2015-04-30 17:31:10 -0400

    Basic chat functionality is up and running at this point

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 var express = require("express");
 var app = express();
 var port = 3500;
- 
-app.get("/", function(req, res){
-    res.send("The web server is up and running!!");
-});
- 
 
-var io = require('socket.io').listen(app.listen(port));
-
-console.log("Listening on port " + port);
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
+ 
 app.get("/", function(req, res){
     res.render("page");
 });
+ 
+var io = require('socket.io').listen(app.listen(port));
 
-app.use(express.static(__dirname + '/public'));
 
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'Welcome to nodeChat!' });
@@ -26,3 +21,4 @@ io.sockets.on('connection', function (socket) {
         io.sockets.emit('message', data);
     });
 });
+console.log("Listening on port " + port);
