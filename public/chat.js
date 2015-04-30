2adc3776558eef4eae3ac00c6ae7e7ce713532a7
Talal Balouch <talal.balouch.ctr@darpa.mil>
2015-04-30 17:45:55 -0400

    Updated chat.js to include username information

--- public/chat.js
+++ public/chat.js
@@ -1,17 +1,19 @@
 window.onload = function() {
  
     var messages = [];
-    var socket = io.connect('http://localhost:3500');
+    var socket = io.connect('http://localhost:3700');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
+    var name = document.getElementById("name");
  
     socket.on('message', function (data) {
         if(data.message) {
-            messages.push(data.message);
+            messages.push(data);
             var html = '';
             for(var i=0; i<messages.length; i++) {
-                html += messages[i] + '<br />';
+                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+                html += messages[i].message + '<br />';
             }
             content.innerHTML = html;
         } else {
@@ -20,8 +22,12 @@ window.onload = function() {
     });
  
     sendButton.onclick = function() {
-        var text = field.value;
-        socket.emit('send', { message: text });
+        if(name.value == "") {
+            alert("Please type your name!");
+        } else {
+            var text = field.value;
+            socket.emit('send', { message: text, username: name.value });
+        }
     };
  
 }
