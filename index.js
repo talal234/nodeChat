b2e04b954a426dec839d9291483883580573c470
Talal Balouch <talalbalouch@Talals-MBP.tbalz.lan>
2015-05-01 01:16:24 -0400

    fixed up index.js due to brokenfront end chat terminal

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname + '/public'));
- 
+
 app.get("/", function(req, res){
-    res.render("page");
+	res.render("page");
 });
- 
+
+
 var io = require('socket.io').listen(app.listen(port));
 
-
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'Welcome to nodeChat!' });
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
-    });
+	socket.emit('message', { message: 'Hello! Welcome to nodeChat' });
+	socket.on('send', function (data) {
+		io.sockets.emit('message', data);
+	});
 });
 console.log("Listening on port " + port);
