ae8c3f0b3b7edaf8d38f5f851a32bcf3e2c5a168
Fareez Ahmed <Fareez.Ahmed@gmail.com>
2015-05-15 12:22:56 -0400

    integrated bootstrap

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 var express = require("express");
 var app = express();
 var port = 3500;
+var path = require('path');
+
+app.use(express.static(__dirname));
 
 
-app.set('views', __dirname + '/tpl');
-app.set('view engine', "jade");
-app.engine('jade', require('jade').__express);
-app.use(express.static(__dirname + '/public'));
-
-app.get("/", function(req, res){
-	res.render("page");
-});
 
 
 var io = require('socket.io').listen(app.listen(port));
